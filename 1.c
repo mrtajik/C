cd1145407a5ad579baf0d30c4cca8997247e7823
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-22 17:27:42 -0400

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-	printf("To C, or not C:That is the question.\n");
+	printf("To C, or not to C:That is the question.\n");
 	return 0;
 }
