1fd9f6a136b1f2e35cdae998416f6df5497c0a86
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-24 13:44:42 -0400

    Data types-integers
    integer types

--- 9_a.c
+++ 9_a.c
@@ -1,13 +1,13 @@
 /* integer types */
 /*
-    short          int  2	-32,768 to 32,767   			%hd
-    unsigned short int	2	0       to 65,535	    		%hu
-          unsigned int	4	0   to 4,294,967,295			%u
-                   int	4	-2,147,483,648 to 2,147,483,647	        %d
-              long int	8	-2,147,483,648 to 2,147,483,647	        %ld
-     unsigned long int	8	0 to 4,294,967,295			%lu
-         long long int	8	-(2^63) to (2^63)-1			%lld
-unsigned long long int	8	0 to 18,446,744,073,709,551,615		%llu	
+    short          int  2	-32,768        to 32,767   			%hd
+    unsigned short int	2	0              to 65,535	    		%hu
+          unsigned int	4	0              to 4,294,967,295			%u
+                   int	4	-2,147,483,648 to 2,147,483,647	        	%d
+              long int	8	-2,147,483,648 to 2,147,483,647	        	%ld
+     unsigned long int	8	0              to 4,294,967,295			%lu
+         long long int	8	-(2^63)        to (2^63)-1			%lld
+unsigned long long int	8	0              to 18,446,744,073,709,551,615	%llu	
 */
 
 /* Getting the sizes of integer types */
