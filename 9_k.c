0462e80e7dd6088cb82ed59736d79dbe6cf008c3
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-24 14:54:54 -0400

    Array
    Arrays-1D

---
+++ 9_k.c
@@ -0,0 +1,16 @@
+/* Arrays */
+/*
+	One-Dimensional Arrays
+*/
+
+#include <stdio.h>
+#define N 10
+
+int main(int argc, char const *argv[])
+{
+	int a[N];
+	a[5]=67; /* assigning value to 5th element of array*/
+
+	printf("%d\n", a[5]);
+
+}
