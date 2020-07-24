2e8ac89c4793f56cd545839f6da5fe94560cbf9e
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-24 10:51:07 -0400

    NEW
    NEW

---
+++ 2_2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	printf("Parkinson's Law: \nWork expands so as to ");
+	printf("fill the time\n");
+	printf("available for its completion.\n");
+	return 0;
+}
