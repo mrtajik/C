fd2a0c38ac67db677fc7efa4f794be6272855cce
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-24 11:19:40 -0400

    Delete 2_2.c

--- 2_2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(int argc, char const *argv[])
-{
-	printf("Parkinson's Law: \nWork expands so as to ");
-	printf("fill the time\n");
-	printf("available for its completion.\n");
-	return 0;
-}
