e5c376672b03b3547d61d662266b24b9572fea9d
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-24 16:06:46 -0400

    Organizing C Program
    Organizing C Program

---
+++ 9_s.c
@@ -0,0 +1,11 @@
+/* Organizing C Program */
+/*
+	Possible ordering in C:
+	-#include directives
+	-#define  directives
+	-Type definitions
+	-Declarations of external variables
+	-Prototypes for functions 
+	-main()
+	-definitions of other functions
+*/
