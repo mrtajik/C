7037464835fa9ee7301e11803587a57d96200e23
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-24 11:06:27 -0400

    Escape Sequences
    Escape Sequences

---
+++ 4.c
@@ -0,0 +1,14 @@
+/*Escape Sequences*/
+/* alert(bell)    \a
+   backspace      \b
+   new line       \n
+   horizontal tab \t */
+
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("Item\tUnit\tPurchase\n\tPrice\tDate\n");
+
+	return 0;
+}
