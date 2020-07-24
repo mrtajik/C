2b8ada6ebf825754428da222df54ff202419ffca
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-24 11:27:00 -0400

    The scanf function
    The scanf function

---
+++ 5.c
@@ -0,0 +1,21 @@
+/*The scanf function*/
+/* %d (or %i) int(base 10)
+   %c         char
+   %f         float
+   %lf        double
+   %s         string
+   %x         hex(base 16)
+*/
+
+#include <stdio.h>
+main()
+{
+	int a = 1022;
+    char b = 'Z';
+    printf("a equals %d \n", a);
+    printf("a equals %c \n", a);
+    printf("b equals %d \n", b);
+    printf("b equals %c \n", b);
+	
+	return 0;
+}	
