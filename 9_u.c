cd0b19e14836c2769eee9afd88405c2b7542825c
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-24 17:03:36 -0400

    Strings
    Strings

---
+++ 9_u.c
@@ -0,0 +1,32 @@
+/* Strings */
+/*
+	String literal - a sequence of characters enclosed 
+	within double quotes.
+
+	String literals may contains same escape sequence.
+
+	Initializing a String variable
+	char date1[8]="July 24";
+
+	As we can see there are 7 charaters. However, C adds \0
+	to the end of array(Yes string is array).
+
+
+	Reading and Writing Strings
+	char str[]="Are we having fun yet?";
+	printf("Value of str: %s\n", str);
+
+	out: Value of str: Are we having fun yet?
+
+	
+
+	C string library
+	string.h - string operations
+
+	The strcpy(String Copy) function
+	The strcat(String Concatenate) function
+	The strcmp(String Compare) function
+	The strlen(String Length) function
+	
+
+*/
