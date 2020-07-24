43ae6088d02feb7c37973b90bf226838a53e0165
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-24 15:29:31 -0400

    Functions
    Functions -void

---
+++ 9_n.c
@@ -0,0 +1,22 @@
+/* Functions*/
+/* VOID */
+
+#include <stdio.h>
+
+void print_count(int n)
+{
+	printf("T minus %d and counting\n", n);
+}
+
+int main(int argc, char const *argv[])
+
+{
+	int i;
+
+	for (int i = 10; i > 0; --i)
+	{
+		print_count(i);
+	}
+	
+	return 0;
+}
