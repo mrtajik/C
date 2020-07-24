612952b7f5fe85d356b2fb1b758129e2a649aedb
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-24 15:50:20 -0400

    local variables

---
+++ 9_p.c
@@ -0,0 +1,14 @@
+/*	local    variables */
+/*
+	Properties:
+	Automatic Storage duration.
+	Block Scope.
+	Also, putting static in declaration of local variables 
+	makes it permanent, during program execution. 
+*/
+
+void sample(void)
+{
+	static int i;
+}
+
