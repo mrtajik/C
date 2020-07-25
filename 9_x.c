30c3d637b4c179f6ebc6fb8eca69b5b90246baaf
MUBINJON SATYMOV <42583585+mrtajik@users.noreply.github.com>
2020-07-24 23:00:08 -0400

    Other things of C
    Other things of C

--- 9_x.c
+++ 9_x.c
@@ -10,7 +10,24 @@
 	qsort
 	
 	NULL pointer
+	
+	Standart libraries of C
+	<assert.h>	Program assertion functions
+	<ctype.h>	Character type functions
+	<locale.h>	Localization functions
+	<math.h>	Mathematics functions
+	<setjmp.h>	Jump functions
+	<signal.h>	Signal handling functions
+	<stdarg.h>	Variable arguments handling functions
+	<stdio.h>	Standard Input/Output functions
+	<stdlib.h>	Standard Utility functions
+	<string.h>	String handling functions
+	<time.h>	Date time functions
+	
+	
+	INPUT/OUTPUT
+	Opening and Closing files
 
 	
-
+	ERROR HANDLING
 */
